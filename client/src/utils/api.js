766cdc36fcb110f54de34886d47057b7080f7a0e
jagoncalvesrodrigues <jagoncalvesrodrigues@gmail.com>
2025-03-19 23:56:46 +0100

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 
 
-const URL = 'http://localhost:3000'
+// const URL = 'http://localhost:3000'
+const URL = 'https://wise-gym-react-lah7.vercel.app'
 const API_URL = '/api/users'
 
 const getUserById = async (user, setUserData) => {
